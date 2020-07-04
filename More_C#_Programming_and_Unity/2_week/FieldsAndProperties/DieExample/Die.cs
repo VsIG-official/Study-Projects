729ea91cbc0aa1fadd442c377e7458edde1729c5
VsIG <Valentyn2202@gmail.com>
2020-07-04 16:54:31 +0300

    one of the exercises

---
+++ More_C#_Programming_and_Unity/2_week/FieldsAndProperties/DieExample/Die.cs
@@ -0,0 +1,38 @@
+﻿namespace DieExample
+{
+    /// <summary>
+    /// A die
+    /// </summary>
+    public class Die
+    {
+        #region Fields
+
+        int numSides;
+        int topSide;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the number of sides
+        /// </summary>
+        /// <value>number of sides</value>
+        public int NumSides
+        {
+            get { return numSides; }
+        }
+
+        /// <summary>
+        /// Gets the top side
+        /// </summary>
+        /// <value>top side</value>
+        public int TopSide
+        {
+            get { return topSide; }
+        }
+
+        #endregion
+
+    }
+}
---
+++ More_C#_Programming_and_Unity/2_week/FieldsAndProperties/DieExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DieExample
+{
+    /// <summary>
+    /// Demonstrates testing a Die class
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Tests the die class
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
