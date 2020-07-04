641cfd0cbf925f0f5cf135831c06c00cffdf7ca5
VsIG <Valentyn2202@gmail.com>
2020-07-04 15:03:29 +0300

    update

---
+++ More_C#_Programming_and_Unity/1_week/Programing Assignment 1/new/Asteroids/Assets/scripts/Ship.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A ship
+/// </summary>
+public class Ship : MonoBehaviour
+{
+    // thrust and rotation support
+    Rigidbody2D rb2D;
+    Vector2 thrustDirection = new Vector2(1, 0);
+    const float ThrustForce = 10;
+    const float RotateDegreesPerSecond = 180;
+
+    // screen wrapping support
+    float colliderRadius;
+
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start()
+	{
+		// saved for efficiency
+        rb2D = GetComponent<Rigidbody2D>();
+        colliderRadius = GetComponent<CircleCollider2D>().radius;
+	}
+	
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update()
+	{
+        // check for rotation input
+        float rotationInput = Input.GetAxis("Rotate");
+        if (rotationInput != 0) {
+
+            // calculate rotation amount and apply rotation
+            float rotationAmount = RotateDegreesPerSecond * Time.deltaTime;
+            if (rotationInput < 0) {
+                rotationAmount *= -1;
+            }
+            transform.Rotate(Vector3.forward, rotationAmount);
+
+            // change thrust direction to match ship rotation
+            float zRotation = transform.eulerAngles.z * Mathf.Deg2Rad;
+            thrustDirection.x = Mathf.Cos(zRotation);
+            thrustDirection.y = Mathf.Sin(zRotation);
+        }
+	}
+
+    /// <summary>
+    /// FixedUpdate is called 50 times per second
+    /// </summary>
+    void FixedUpdate()
+    {
+        // thrust as appropriate
+        if (Input.GetAxis("Thrust") != 0)
+        {
+            rb2D.AddForce(ThrustForce * thrustDirection,
+                ForceMode2D.Force);
+        }
+    }
+
+    /// <summary>
+    /// Called when the game object becomes invisible to the camera
+    /// </summary>
+    void OnBecameInvisible()
+    {
+        Vector2 position = transform.position;
+
+        // check left, right, top, and bottom sides
+        if (position.x + colliderRadius < ScreenUtils.ScreenLeft ||
+            position.x - colliderRadius > ScreenUtils.ScreenRight)
+        {
+            position.x *= -1;
+        }
+        if (position.y - colliderRadius > ScreenUtils.ScreenTop ||
+            position.y + colliderRadius < ScreenUtils.ScreenBottom)
+        {
+            position.y *= -1;
+        }
+
+        // move ship
+        transform.position = position;
+    }
+}
