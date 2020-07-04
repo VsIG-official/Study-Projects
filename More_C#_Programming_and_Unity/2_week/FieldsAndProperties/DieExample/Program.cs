c6accf4ba6228baebb71e04b23d352c8646af572
VsIG <Valentyn2202@gmail.com>
2020-07-04 22:19:25 +0300

    Before prog.assignment

--- More_C#_Programming_and_Unity/2_week/FieldsAndProperties/DieExample/Program.cs
+++ More_C#_Programming_and_Unity/2_week/FieldsAndProperties/DieExample/Program.cs
@@ -11,9 +11,34 @@ namespace DieExample
         /// Tests the die class
         /// </summary>
         /// <param name="args">command-line arguments</param>
-        static void Main(string[] args)
+        static void Main()
         {
+            // test standard die
+            Die standardDie = new Die();
+            Console.WriteLine("Standard Die");
+            Console.WriteLine("------------");
+            Console.WriteLine("Number of sides: " + standardDie.NumSides);
+            Console.WriteLine("Top side: " + standardDie.TopSide);
+
+            //roll the die
+            standardDie.Roll();
+            Console.WriteLine("Top side: " + standardDie.TopSide);
             Console.WriteLine();
+
+            // test d20 die
+            Die d20Die = new Die(20);
+            Console.WriteLine("D20 Die");
+            Console.WriteLine("-------");
+            Console.WriteLine("Number of sides: " + d20Die.NumSides);
+            Console.WriteLine("Top side: " + d20Die.TopSide);
+
+            //roll the die
+            d20Die.Roll();
+            Console.WriteLine("Top side: " + d20Die.TopSide);
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.Read();
         }
     }
 }
