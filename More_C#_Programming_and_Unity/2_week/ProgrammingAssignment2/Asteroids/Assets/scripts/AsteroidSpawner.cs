84421340649445d8f4260cb4a6d333ef9be0e003
VsIG <Valentyn2202@gmail.com>
2020-07-05 18:36:40 +0300

    beginning of the 3-rd week

---
+++ More_C#_Programming_and_Unity/2_week/ProgrammingAssignment2/Asteroids/Assets/scripts/AsteroidSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class for spawning asteroids
+/// </summary>
+public class AsteroidSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefabAsteroid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject asteroid = Instantiate(prefabAsteroid);
+        float radius = asteroid.GetComponent<CircleCollider2D>().radius;
+        Destroy(asteroid);
+
+        GameObject asteroidLeft = Instantiate(prefabAsteroid);
+        asteroidLeft.GetComponent<Asteroid>().Initialize(Direction.Left,new Vector2
+            (ScreenUtils.ScreenRight + radius,
+            (ScreenUtils.ScreenTop + ScreenUtils.ScreenBottom) / 2));
+
+        GameObject asteroidRight = Instantiate(prefabAsteroid);
+        asteroidRight.GetComponent<Asteroid>().Initialize(Direction.Right, new Vector2
+            (ScreenUtils.ScreenLeft - radius,
+            (ScreenUtils.ScreenTop + ScreenUtils.ScreenBottom) / 2));
+
+        GameObject asteroidTop = Instantiate(prefabAsteroid);
+        asteroidTop.GetComponent<Asteroid>().Initialize(Direction.Down, new Vector2
+            ((ScreenUtils.ScreenLeft + ScreenUtils.ScreenRight) / 2,
+            ScreenUtils.ScreenTop + radius));
+
+        GameObject asteroidDown = Instantiate(prefabAsteroid);
+        asteroidDown.GetComponent<Asteroid>().Initialize(Direction.Up, new Vector2
+            ((ScreenUtils.ScreenLeft + ScreenUtils.ScreenRight) / 2,
+            ScreenUtils.ScreenBottom - radius));
+    }
+}
