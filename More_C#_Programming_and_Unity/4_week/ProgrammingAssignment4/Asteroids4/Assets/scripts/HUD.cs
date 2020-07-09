54f748b9559ba29772e3c00ed3bd51663517f4f6
VsIG <Valentyn2202@gmail.com>
2020-07-09 13:55:17 +0300

    Before last step

---
+++ More_C#_Programming_and_Unity/4_week/ProgrammingAssignment4/Asteroids4/Assets/scripts/HUD.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// HUD script
+/// </summary>
+public class HUD : MonoBehaviour
+{
+    [SerializeField]
+    Text scoreText;
+
+    private float elapsedSeconds = 0f;
+    private bool timerIsRunning = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    scoreText.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    if (timerIsRunning)
+	    {
+			elapsedSeconds += Time.deltaTime;
+			scoreText.text = elapsedSeconds.ToString("0");
+	    }
+    }
+
+    public void StopGamerTimer()
+    {
+	    timerIsRunning = false;
+    }
+}
