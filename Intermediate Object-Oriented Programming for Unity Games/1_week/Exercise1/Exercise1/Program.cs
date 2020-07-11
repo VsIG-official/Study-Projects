70b9a09319807f45d190625a23716db155741c06
VsIG <Valentyn2202@gmail.com>
2020-07-11 17:33:09 +0300

    chore: add codes some exercises

---
+++ Intermediate Object-Oriented Programming for Unity Games/1_week/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise1
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("Enter some int score");
+			int score = int.Parse(Console.ReadLine());
+
+			try
+			{
+
+			}
+			catch (FormatException fe)
+			{
+				Console.WriteLine("Non-integer input");
+				throw;
+			}
+			catch (OverflowException oe)
+			{
+				Console.WriteLine("integer is out of range");
+				throw;
+			}
+			finally
+			{
+				Console.WriteLine("Oh shit,a rat!");
+			}
+		}
+	}
+}
