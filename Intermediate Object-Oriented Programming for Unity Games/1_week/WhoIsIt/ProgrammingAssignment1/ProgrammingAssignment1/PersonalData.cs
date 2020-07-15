13f0c1ce9e01cbf93276941847bd189136a86c33
VsIG <Valentyn2202@gmail.com>
2020-07-15 16:46:33 +0300

    feat: add new exercises and solution

---
+++ Intermediate Object-Oriented Programming for Unity Games/1_week/WhoIsIt/ProgrammingAssignment1/ProgrammingAssignment1/PersonalData.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment1
+{
+    /// <summary>
+    /// A class holding personal data for a person
+    /// </summary>
+    public class PersonalData
+    {
+        #region Fields
+
+        // declare your fields here
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the person's first name
+        /// </summary>
+        public string FirstName
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's middle name
+        /// </summary>
+        public string MiddleName
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's last name
+        /// </summary>
+        public string LastName
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's street address
+        /// </summary>
+        public string StreetAddress
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's city or town
+        /// </summary>
+        public string City
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's state or province
+        /// </summary>
+        public string State
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's postal code
+        /// </summary>
+        public string PostalCode
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's country
+        /// </summary>
+        public string Country
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Gets the person's phone number (digits only, no
+        /// parentheses, spaces, or dashes)
+        /// </summary>
+        public string PhoneNumber
+        {
+            get
+            {
+                // delete code below and replace with correct code
+                return "";
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// Reads personal data from a file. If the file
+        /// read fails, the object contains an empty string for all
+        /// the personal data
+        /// </summary>
+        /// <param name="fileName">name of file holding personal data</param>
+        public PersonalData(string fileName)
+        {
+            // your code can assume we know the order in which the
+            // values appear in the string; it's the same order as
+            // they're listed for the properties above. We could do
+            // something more complicated with the names and values,
+            // but that's not necessary here
+
+            // IMPORTANT: The mono compiler the automated grader uses
+            // does NOT support the string Split method. You have to
+            // use the IndexOf method to find comma locations and the
+            // Substring method to chop off the front of the string
+            // after you extract each value to extract and save the
+            // personal data
+        }
+
+        #endregion
+    }
+}
