a0a5d379671c2eac0ad485496fc7873845a8f01c
VsIG <Valentyn2202@gmail.com>
2020-07-15 20:02:36 +0300

    Feat: add almost entire solution but have prob with site

--- Intermediate Object-Oriented Programming for Unity Games/1_week/WhoIsIt/ProgrammingAssignment1/ProgrammingAssignment1/PersonalData.cs
+++ Intermediate Object-Oriented Programming for Unity Games/1_week/WhoIsIt/ProgrammingAssignment1/ProgrammingAssignment1/PersonalData.cs
@@ -1,161 +1,109 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProgrammingAssignment1
 {
-    /// <summary>
-    /// A class holding personal data for a person
-    /// </summary>
-    public class PersonalData
-    {
-        #region Fields
+	/// <summary>
+	/// A class holding personal data for a person
+	/// </summary>
+	public class PersonalData
+	{
+		#region Properties
 
-        // declare your fields here
+		/// <summary>
+		/// Gets the person's first name
+		/// </summary>
+		public string FirstName { get; } = "";
 
-        #endregion
+		/// <summary>
+		/// Gets the person's middle name
+		/// </summary>
+		public string MiddleName { get; } = "";
 
-        #region Properties
+		/// <summary>
+		/// Gets the person's last name
+		/// </summary>
+		public string LastName { get; } = "";
 
-        /// <summary>
-        /// Gets the person's first name
-        /// </summary>
-        public string FirstName
-        {
-            get
-            {
-                // delete code below and replace with correct code
-                return "";
-            }
-        }
+		/// <summary>
+		/// Gets the person's street address
+		/// </summary>
+		public string StreetAddress { get; } = "";
 
-        /// <summary>
-        /// Gets the person's middle name
-        /// </summary>
-        public string MiddleName
-        {
-            get
-            {
-                // delete code below and replace with correct code
-                return "";
-            }
-        }
+		/// <summary>
+		/// Gets the person's city or town
+		/// </summary>
+		public string City { get; } = "";
 
-        /// <summary>
-        /// Gets the person's last name
-        /// </summary>
-        public string LastName
-        {
-            get
-            {
-                // delete code below and replace with correct code
-                return "";
-            }
-        }
+		/// <summary>
+		/// Gets the person's state or province
+		/// </summary>
+		public string State { get; } = "";
 
-        /// <summary>
-        /// Gets the person's street address
-        /// </summary>
-        public string StreetAddress
-        {
-            get
-            {
-                // delete code below and replace with correct code
-                return "";
-            }
-        }
+		/// <summary>
+		/// Gets the person's postal code
+		/// </summary>
+		public string PostalCode { get; } = "";
 
-        /// <summary>
-        /// Gets the person's city or town
-        /// </summary>
-        public string City
-        {
-            get
-            {
-                // delete code below and replace with correct code
-                return "";
-            }
-        }
+		/// <summary>
+		/// Gets the person's country
+		/// </summary>
+		public string Country { get; } = "";
 
-        /// <summary>
-        /// Gets the person's state or province
-        /// </summary>
-        public string State
-        {
-            get
-            {
-                // delete code below and replace with correct code
-                return "";
-            }
-        }
+		/// <summary>
+		/// Gets the person's phone number (digits only, no
+		/// parentheses, spaces, or dashes)
+		/// </summary>
+		public string PhoneNumber { get; } = "";
 
-        /// <summary>
-        /// Gets the person's postal code
-        /// </summary>
-        public string PostalCode
-        {
-            get
-            {
-                // delete code below and replace with correct code
-                return "";
-            }
-        }
+		#endregion
 
-        /// <summary>
-        /// Gets the person's country
-        /// </summary>
-        public string Country
-        {
-            get
-            {
-                // delete code below and replace with correct code
-                return "";
-            }
-        }
+		#region Constructor
 
-        /// <summary>
-        /// Gets the person's phone number (digits only, no
-        /// parentheses, spaces, or dashes)
-        /// </summary>
-        public string PhoneNumber
-        {
-            get
-            {
-                // delete code below and replace with correct code
-                return "";
-            }
-        }
+		/// <summary>
+		/// Constructor
+		/// Reads personal data from a file. If the file
+		/// read fails, the object contains an empty string for all
+		/// the personal data
+		/// </summary>
+		/// <param name="fileName">name of file holding personal data</param>
+		public PersonalData(string fileName)
+		{
+			StreamReader input = null;
+			string line = null;
+			try
+			{
+				input = File.OpenText(fileName);
 
-        #endregion
+				line = input.ReadLine();
+			}
+			catch (Exception e)
+			{
+				if (input == null)
+				{
+					Console.WriteLine(e.Message);
+				}
+			}
+			finally
+			{
+				if (input != null)
+				{
+					input.Close();
+				}
+			}
 
-        #region Constructor
+			string[] CSVvalues = line.Split(',');
+			FirstName = CSVvalues[0];
+			MiddleName = CSVvalues[1];
+			LastName = CSVvalues[2];
+			StreetAddress = CSVvalues[3];
+			City = CSVvalues[4];
+			State = CSVvalues[5];
+			PostalCode = CSVvalues[6];
+			Country = CSVvalues[7];
+			PhoneNumber = CSVvalues[8];
+		}
 
-        /// <summary>
-        /// Constructor
-        /// Reads personal data from a file. If the file
-        /// read fails, the object contains an empty string for all
-        /// the personal data
-        /// </summary>
-        /// <param name="fileName">name of file holding personal data</param>
-        public PersonalData(string fileName)
-        {
-            // your code can assume we know the order in which the
-            // values appear in the string; it's the same order as
-            // they're listed for the properties above. We could do
-            // something more complicated with the names and values,
-            // but that's not necessary here
-
-            // IMPORTANT: The mono compiler the automated grader uses
-            // does NOT support the string Split method. You have to
-            // use the IndexOf method to find comma locations and the
-            // Substring method to chop off the front of the string
-            // after you extract each value to extract and save the
-            // personal data
-        }
-
-        #endregion
-    }
+		#endregion
+	}
 }
