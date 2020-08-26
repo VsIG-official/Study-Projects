427126ec7a3d2ad97fa02d6cf3b1b821187437a3
VsIG <Valentyn2202@gmail.com>
2020-08-26 21:45:56 +0300

    📝: add all lecture code and exercises (not started)

---
+++ Data Structures and Design Patterns for Game Developers/2week/Exercise5/Solution/LinkedLists/LinkedLists/SortedLinkedList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    /// <summary>
+    /// A sorted linked list
+    /// </summary>
+    class SortedLinkedList<T> : LinkedList<T> where T:IComparable
+    {
+        #region Constructors
+
+        public SortedLinkedList() : base()
+        {
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Adds the given item to the list
+        /// </summary>
+        /// <param name="item">item to add</param>
+        public override void Add(T item)
+        {
+            // adding to empty list
+            if (head == null)
+            {
+                head = new LinkedListNode<T>(item, null, null);
+            }
+            else if (head.Value.CompareTo(item) > 0)
+            {
+                // adding before head
+                head = new LinkedListNode<T>(item, head, null);
+                head.Next.Previous = head;
+            }
+            else
+            {
+                // find place to add new node
+                // need previous node for adding to end of list
+                LinkedListNode<T> previousNode = head;
+                LinkedListNode<T> currentNode = head.Next;
+                while (currentNode != null &&
+                    currentNode.Value.CompareTo(item) < 0)
+                {
+                    currentNode = currentNode.Next;
+                }
+
+                // check for adding to end of list
+                if (currentNode == null)
+                {
+                    previousNode.Next = new LinkedListNode<T>(item,
+                        null, previousNode);
+                }
+                else
+                {
+                    // link in new node between previous node and current node
+                    currentNode.Previous.Next = new LinkedListNode<T>(
+                        item, currentNode, null);
+                }
+            }
+            count++;
+        }
+
+        #endregion
+    }
+}
+
---
+++ Data Structures and Design Patterns for Game Developers/2week/Exercise7/Solution/GraphSearching/GraphSearching/PathNodeInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphSearching
+{
+    /// <summary>
+    /// Info about a node along a search path
+    /// </summary>
+    /// <typeparam name="T">type stored in graph node</typeparam>
+    class PathNodeInfo<T>
+    {
+        #region Fields
+
+        GraphNode<T> previous;
+        float weight;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates an info node with the given previous node
+        /// </summary>
+        /// <param name="previous">previous node in path</param>
+        /// <param name="weight">weight of the edge to the previous node</param>
+        public PathNodeInfo(GraphNode<T> previous, float weight)
+        {
+            this.previous = previous;
+            this.weight = weight;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the previous graph node in the path
+        /// </summary>
+        public GraphNode<T> Previous
+        {
+            get { return previous; }
+        }
+
+        /// <summary>
+        /// Gets the weight of the edge to the previous node
+        /// </summary>
+        public float Weight
+        {
+            get { return weight; }
+        }
+
+        #endregion
+    }
+}
