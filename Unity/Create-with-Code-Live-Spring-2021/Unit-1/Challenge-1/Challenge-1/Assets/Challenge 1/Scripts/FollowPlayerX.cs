d2b4ca8568dc518283e5849e98a41269b8dae4e0
Valentyn-Dominskyi <valentyn2202@gmail.com>
2021-06-13 16:23:38 +0300

    🎉: end challenge-1

---
+++ Unity/Create-with-Code-Live-Spring-2021/Unit-1/Challenge-1/Challenge-1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    [SerializeField]
+    private Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Unity/Create-with-Code-Live-Spring-2021/Unit-1/Challenge-1/Challenge-1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * verticalInput * rotationSpeed * Time.deltaTime);
+    }
+}
