ddfeb461fdf2b21ae462180ba12e3d6cd458363d
Valentyn-Dominskyi <valentyn2202@gmail.com>
2021-06-15 21:40:39 +0300

    🎉: start chapter 1

---
+++ CSharp/C# 9 and .NET 5 – Modern Cross-Platform Development Fifth Edition/Chapter-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// #error version
+
+namespace Chapter_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
