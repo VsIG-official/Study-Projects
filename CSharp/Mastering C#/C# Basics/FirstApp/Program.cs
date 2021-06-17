931c058c154245431cd05e5047c8378c763dfb61
Valentyn-Dominskyi <valentyn2202@gmail.com>
2021-06-17 19:24:00 +0300

    ✨: start syntax errors

--- CSharp/Mastering C#/C# Basics/FirstApp/Program.cs
+++ CSharp/Mastering C#/C# Basics/FirstApp/Program.cs
@@ -6,14 +6,26 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
-			var a = 1;
-			var b = 2;
-			var c = a + b;
+			// s = ut + 0.5att
+			var u = 50;
+			var aNew = -9.81;
+			var t = 2;
 
-			Console.WriteLine("1 + 2 = " + a + b);
+			var s = u * t + 0.5 * aNew * t * t;
+
+			Console.WriteLine(s);
+
+			int a = 1;
+			int b = 2;
+			string c = "1 + 2 = " + (a + b);
+
 			Console.WriteLine(c);
 
 			Console.WriteLine();
+
+			double dbl = 5.3333;
+			bool isenabled = true;
+			var unknown = 5;
 
 			Console.WriteLine("Hello, World!");
 			Console.WriteLine("What's Your name?");
