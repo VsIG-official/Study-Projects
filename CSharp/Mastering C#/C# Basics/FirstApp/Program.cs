69cff0d97fc84cf105a1778b019f5a58f34bf157
Valentyn-Dominskyi <valentyn2202@gmail.com>
2021-06-17 19:16:26 +0300

    ✨: add simple variables

--- CSharp/Mastering C#/C# Basics/FirstApp/Program.cs
+++ CSharp/Mastering C#/C# Basics/FirstApp/Program.cs
@@ -6,9 +6,19 @@ namespace FirstApp
 	{
 		static void Main(string[] args)
 		{
+			var a = 1;
+			var b = 2;
+			var c = a + b;
+
+			Console.WriteLine("1 + 2 = " + a + b);
+			Console.WriteLine(c);
+
+			Console.WriteLine();
+
 			Console.WriteLine("Hello, World!");
 			Console.WriteLine("What's Your name?");
-			Console.WriteLine(Console.ReadLine());
+			var Text = Console.ReadLine();
+			Console.WriteLine("Hello, " + Text);
 			Console.ReadLine();
 		}
 	}
