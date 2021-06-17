314d3f874051523dba7b6f4a9ede82f2fa00e56c
Valentyn-Dominskyi <valentyn2202@gmail.com>
2021-06-17 19:03:45 +0300

    ✨: add simple I/O

---
+++ CSharp/Mastering C#/C# Basics/FirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			Console.WriteLine("What's Your name?");
+			Console.WriteLine(Console.ReadLine());
+			Console.ReadLine();
+		}
+	}
+}
