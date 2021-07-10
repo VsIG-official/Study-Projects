3d72b7477eca131cfd82b42b6db5330907ad3c71
Valentyn-Dominskyi <valentyn.dominskyi.official@gmail.com>
2021-07-10 14:19:26 +0300

    ✨: add Billiards

---
+++ CSharp/ULearn/Billiards/BilliardsTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Billiards
+{
+    public static class BilliardsTask
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="directionRadians">Угол направления движения шара</param>
+        /// <param name="wallInclinationRadians">Угол</param>
+        /// <returns></returns>
+        public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        {
+            return (wallInclinationRadians - directionRadians) + wallInclinationRadians;
+        }
+    }
+}
