86460ef8ba0ed95edc2c077e525cd7a9a725db28
Valentyn-Dominskyi <valentyn.dominskyi.official@gmail.com>
2021-07-10 16:43:31 +0300

    ✨: add Percents

---
+++ CSharp/ULearn/Percents/Percents/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Percents
+{
+    internal class Program
+	{
+		static void Main()
+		{
+            Console.WriteLine("Enter Money to put, percents and months:");
+            string enterValues = Console.ReadLine();
+            Console.WriteLine(Calculate(enterValues)); 
+		}
+
+        public static double Calculate(string userInput)
+        {
+            string[] subs = userInput.Split(' ');
+            double money = double.Parse(subs[0]);
+            double percent = double.Parse(subs[1]);
+            double months = double.Parse(subs[2]);
+
+            return money * Math.Pow(1 + (percent / (12 * 100)), months);
+        }
+    }
+}
