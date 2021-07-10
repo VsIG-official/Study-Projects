db697d90ab01510720b29096c58fbca098906493
Valentyn-Dominskyi <valentyn.dominskyi.official@gmail.com>
2021-07-10 23:18:52 +0300

    ✨: end "Risovatel"

--- CSharp/ULearn/Risovatel/DrawingProgram.cs
+++ CSharp/ULearn/Risovatel/DrawingProgram.cs
@@ -4,13 +4,13 @@ using System.Drawing.Drawing2D;
 
 namespace RefactorMe
 {
-    internal class Risovatel
+    internal class Drawer
     {
         private static float s_xPos;
         private static float s_yPos;
         private static Graphics s_graphics;
-        private const float FirstSidePosition = 0.375f;
-        private const float SecondSidePosition = 0.4f;
+        public const float FirstSidePosition = 0.375f;
+        public const float SecondSidePosition = 0.04f;
 
         public static void Initialize(Graphics newGraphics)
         {
@@ -51,16 +51,20 @@ namespace RefactorMe
         public static void DrawSide(Pen color, int distance,
             int multiplier = 1, int divider = 1)
         {
-            DrawPartOfTheSide(color, distance * 0.375f, multiplier * Math.PI / divider);
-            DrawPartOfTheSide(color, distance * 0.04f * Math.Sqrt(2),
+            DrawPartOfTheSide(color, distance * FirstSidePosition,
+                multiplier * Math.PI / divider);
+            DrawPartOfTheSide(color, distance * SecondSidePosition
+                * Math.Sqrt(2),
                 (multiplier * Math.PI / divider) + Math.PI / 4);
-            DrawPartOfTheSide(color, distance * 0.375f, (multiplier
+            DrawPartOfTheSide(color, distance * FirstSidePosition, (multiplier
                 * Math.PI /divider) + Math.PI);
-            DrawPartOfTheSide(color, distance * 0.375f - distance * 0.04f,
+            DrawPartOfTheSide(color, distance * FirstSidePosition
+                - distance * SecondSidePosition,
                 (multiplier * Math.PI / divider) + Math.PI / 2);
 
-            ChangePosition(distance * 0.04f, (multiplier * Math.PI / divider) - Math.PI);
-            ChangePosition(distance * 0.04f * Math.Sqrt(2),
+            ChangePosition(distance * SecondSidePosition, (multiplier
+                * Math.PI / divider) - Math.PI);
+            ChangePosition(distance * SecondSidePosition * Math.Sqrt(2),
                 (multiplier * Math.PI / divider) + 3 * Math.PI / 4);
         }
 
@@ -75,19 +79,23 @@ namespace RefactorMe
 
     public class ImpossibleSquare
     {
-        public static void Draw(int width, int heigth, double angle, Graphics graphic)
+        public static void Draw(int width, int height, double angle, Graphics graphic)
         {
-            Risovatel.Initialize(graphic);
+            Drawer.Initialize(graphic);
 
-            int distance = Math.Min(width, heigth);
+            int distance = Math.Min(width, height);
 
-            double diagonalLength = Math.Sqrt(2) * (distance * 0.375f + distance * 0.04f) / 2;
-            float x0 = (float)(diagonalLength * Math.Cos(Math.PI / 4 + Math.PI)) + width / 2f;
-            float y0 = (float)(diagonalLength * Math.Sin(Math.PI / 4 + Math.PI)) + heigth / 2f;
+            double diagonalLength = Math.Sqrt(2) * (distance
+                * Drawer.FirstSidePosition + distance
+                * Drawer.SecondSidePosition) / 2;
+            float x0 = (float)(diagonalLength * Math.Cos(Math.PI
+                / 4 + Math.PI)) + width / 2f;
+            float y0 = (float)(diagonalLength * Math.Sin(Math.PI
+                / 4 + Math.PI)) + height / 2f;
 
-            Risovatel.SetPosition(x0, y0);
+            Drawer.SetPosition(x0, y0);
 
-            Risovatel.DrawSquare(Pens.Yellow, distance);
+            Drawer.DrawSquare(Pens.Yellow, distance);
         }
     }
 }
--- CSharp/ULearn/Risovatel/Properties/AssemblyInfo.cs
+++ CSharp/ULearn/Risovatel/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("Risovatel")]
+[assembly: AssemblyTitle("Drawer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Risovatel")]
+[assembly: AssemblyProduct("Drawer")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
