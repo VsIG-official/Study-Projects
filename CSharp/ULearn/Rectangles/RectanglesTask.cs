b4c11adf2e72af565ec2e8b638a062c48c2ca3e4
Valentyn-Dominskyi <valentyn.dominskyi.official@gmail.com>
2021-07-15 22:59:16 +0300

    ✨: add two methods

--- CSharp/ULearn/Rectangles/RectanglesTask.cs
+++ CSharp/ULearn/Rectangles/RectanglesTask.cs
@@ -8,15 +8,17 @@ namespace Rectangles
 		// Пересекаются ли два прямоугольника (пересечение только по границе также считается пересечением)
 		public static bool AreIntersected(Rectangle r1, Rectangle r2)
 		{
-            return (r2.Right >= r1.Left && r2.Left <= r2.Right)
-                && (r2.Bottom >= r1.Top && r2.Top <= r1.Bottom);
+            return r2.Right >= r1.Left && r2.Left <= r2.Right
+                && r2.Bottom >= r1.Top && r2.Top <= r1.Bottom;
 		}
 
 		// Площадь пересечения прямоугольников
 		public static int IntersectionSquare(Rectangle r1, Rectangle r2)
 		{
-			return 0;
-		}
+            int x_overlap = Math.Max(0, Math.Min(r1.Right, r2.Right) - Math.Max(r1.Left, r2.Left));
+            int y_overlap = Math.Max(0, Math.Min(r1.Bottom, r2.Bottom) - Math.Max(r1.Top, r2.Top));
+            return x_overlap * y_overlap;
+        }
 
 		// Если один из прямоугольников целиком находится внутри другого — вернуть номер (с нуля) внутреннего.
 		// Иначе вернуть -1
