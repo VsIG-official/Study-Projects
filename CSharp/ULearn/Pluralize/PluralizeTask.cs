9bf1fde4b197d0d42e9faaf2b2411658229d825b
Valentyn-Dominskyi <valentyn.dominskyi.official@gmail.com>
2021-07-15 17:56:45 +0300

    ✨: end "Pluralize"

--- CSharp/ULearn/Pluralize/PluralizeTask.cs
+++ CSharp/ULearn/Pluralize/PluralizeTask.cs
@@ -1,18 +1,22 @@
 ﻿namespace Pluralize
 {
-	public static class PluralizeTask
-	{
-		public static string PluralizeRubles(int count)
-		{
-            if (count % 10 == 1)
+    public static class PluralizeTask
+    {
+        public static string PluralizeRubles(int count)
+        {
+            if (count % 100 >= 11 && count % 100 <= 20)
+            {
+                return "рублей";
+            }
+            else if (count % 10 == 1)
             {
                 return "рубль";
             }
-            else if (count % 10 <= 4 && count % 10 != 0)
+            else if (count % 10 >= 2 && count % 10 <= 4)
             {
                 return "рубля";
             }
             return "рублей";
-		}
-	}
+        }
+    }
 }
