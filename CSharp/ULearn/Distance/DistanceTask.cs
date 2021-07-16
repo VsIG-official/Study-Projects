888cdb3f1f95b3eeedb822c12036ebfbeafa5b08
Valentyn-Dominskyi <valentyn.dominskyi.official@gmail.com>
2021-07-16 13:52:07 +0300

    ✨: end "Distance"

--- CSharp/ULearn/Distance/DistanceTask.cs
+++ CSharp/ULearn/Distance/DistanceTask.cs
@@ -4,10 +4,44 @@ namespace DistanceTask
 {
 	public static class DistanceTask
 	{
-		// Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
-		public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
+		public static double GetDistanceToSegment(double ax, double ay,
+            double bx, double by, double x, double y)
 		{
-			return 0.0;
-		}
-	}
+            double fromAXtoX = x - ax;
+            double fromBXtoX = y - ay;
+            double axAltitude  = bx - ax;
+            double bxLeftHorizont = by - ay;
+
+            double dot = fromAXtoX * axAltitude + fromBXtoX * bxLeftHorizont;
+            double len_sq = axAltitude  * axAltitude  + bxLeftHorizont * bxLeftHorizont;
+            double param = -1;
+
+            if (len_sq != 0)
+            {
+                param = dot / len_sq;
+            }
+
+            double xx, yy;
+
+            if (param < 0)
+            {
+                xx = ax;
+                yy = ay;
+            }
+            else if (param > 1)
+            {
+                xx = bx;
+                yy = by;
+            }
+            else
+            {
+                xx = ax + param * axAltitude ;
+                yy = ay + param * bxLeftHorizont;
+            }
+
+            double dx = x - xx;
+            double dy = y - yy;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+    }
 }
