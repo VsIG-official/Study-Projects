4570f0d73b3c950bfe4945f3f5dfeb64cc4cc08d
Valentyn-Dominskyi <valentyn.dominskyi.official@gmail.com>
2021-07-18 17:15:39 +0300

    ✨: add first implementation of snake maze

--- CSharp/ULearn/Mazes/SnakeMazeTask.cs
+++ CSharp/ULearn/Mazes/SnakeMazeTask.cs
@@ -4,6 +4,32 @@
 	{
 		public static void MoveOut(Robot robot, int width, int height)
 		{
-		}
-	}
+            int timesToTurn = (height - 2) / 2;
+
+            for (int i = 0; i < timesToTurn + 1; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    MoveTo(robot, Direction.Right, width);
+                }
+                else
+                {
+                    MoveTo(robot, Direction.Left, width);
+                }
+
+                if (i != timesToTurn)
+                {
+                    MoveTo(robot, Direction.Down, 5);
+                }
+            }
+        }
+
+        private static void MoveTo(Robot robot, Direction dir, int mazeBounds)
+        {
+            for (var i = 0; i < mazeBounds - 3; i++)
+            {
+                robot.MoveTo(dir);
+            }
+        }
+    }
 }
