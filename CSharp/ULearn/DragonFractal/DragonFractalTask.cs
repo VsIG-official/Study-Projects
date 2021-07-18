68060fdac82cc1ce7f9d86ff0b5e2bfbef4f4d9e
Valentyn-Dominskyi <valentyn.dominskyi.official@gmail.com>
2021-07-18 19:20:13 +0300

    ✨: add Dragon curves

---
+++ CSharp/ULearn/DragonFractal/DragonFractalTask.cs
@@ -0,0 +1,34 @@
+﻿// В этом пространстве имен содержатся средства для работы с изображениями. 
+// Чтобы оно стало доступно, в проект был подключен Reference на сборку System.Drawing.dll
+using System;
+using System.Drawing;
+
+namespace Fractals
+{
+	internal static class DragonFractalTask
+	{
+		public static void DrawDragonFractal(Pixels pixels, int iterationsCount, int seed)
+		{
+            var randGenerator = new Random(seed);
+			/*
+			Начните с точки (1, 0)
+			Создайте генератор рандомных чисел с сидом seed
+			
+			На каждой итерации:
+
+			1. Выберите случайно одно из следующих преобразований и примените его к текущей точке:
+
+				Преобразование 1. (поворот на 45° и сжатие в sqrt(2) раз):
+				x' = (x · cos(45°) - y · sin(45°)) / sqrt(2)
+				y' = (x · sin(45°) + y · cos(45°)) / sqrt(2)
+
+				Преобразование 2. (поворот на 135°, сжатие в sqrt(2) раз, сдвиг по X на единицу):
+				x' = (x · cos(135°) - y · sin(135°)) / sqrt(2) + 1
+				y' = (x · sin(135°) + y · cos(135°)) / sqrt(2)
+		
+			2. Нарисуйте текущую точку методом pixels.SetPixel(x, y)
+
+			*/
+		}
+	}
+}
