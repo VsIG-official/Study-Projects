2b9087c9b6b38fd5b7b1646fe913670960d74edf
Valentyn-Dominskyi <valentyn.dominskyi.official@gmail.com>
2021-07-18 16:06:38 +0300

    ✨: add empty Maze

--- CSharp/ULearn/Mazes/EmptyMazeTask.cs
+++ CSharp/ULearn/Mazes/EmptyMazeTask.cs
@@ -4,6 +4,24 @@
 	{
 		public static void MoveOut(Robot robot, int width, int height)
 		{
-		}
-	}
+            MoveBottom(robot, height);
+            MoveRight(robot, width);
+        }
+
+        private static void MoveBottom(Robot robot, int mazeHeight)
+        {
+            for (var i = 0; i < mazeHeight - 3; i++)
+            {
+                robot.MoveTo(Direction.Down);
+            }
+        }
+
+        private static void MoveRight(Robot robot, int mazeWidth)
+        {
+            for (var i = 0; i < mazeWidth - 3; i++)
+            {
+                robot.MoveTo(Direction.Right);
+            }
+        }
+    }
 }
