09433b98e895ab1804f7952221fff8783d2afc53
Valentyn-Dominskyi <valentyn.dominskyi.official@gmail.com>
2021-07-18 16:09:47 +0300

    ♻️: replace two methods with one

--- CSharp/ULearn/Mazes/EmptyMazeTask.cs
+++ CSharp/ULearn/Mazes/EmptyMazeTask.cs
@@ -4,24 +4,32 @@
 	{
 		public static void MoveOut(Robot robot, int width, int height)
 		{
-            MoveBottom(robot, height);
-            MoveRight(robot, width);
+            MoveTo(robot, Direction.Down, height);
+            MoveTo(robot, Direction.Right, width);
         }
 
-        private static void MoveBottom(Robot robot, int mazeHeight)
+        private static void MoveTo(Robot robot, Direction dir, int mazeBounds)
         {
-            for (var i = 0; i < mazeHeight - 3; i++)
+            for (var i = 0; i < mazeBounds - 3; i++)
             {
-                robot.MoveTo(Direction.Down);
+                robot.MoveTo(dir);
             }
         }
 
-        private static void MoveRight(Robot robot, int mazeWidth)
-        {
-            for (var i = 0; i < mazeWidth - 3; i++)
-            {
-                robot.MoveTo(Direction.Right);
-            }
-        }
+        //private static void MoveBottom(Robot robot, int mazeHeight)
+        //{
+        //    for (var i = 0; i < mazeHeight - 3; i++)
+        //    {
+        //        robot.MoveTo(Direction.Down);
+        //    }
+        //}
+
+        //private static void MoveRight(Robot robot, int mazeWidth)
+        //{
+        //    for (var i = 0; i < mazeWidth - 3; i++)
+        //    {
+        //        robot.MoveTo(Direction.Right);
+        //    }
+        //}
     }
 }
